73cbe05682f9f374a22426b7907c7d918a8a2071
Alex Spencer <alexander.j.spencer@gmail.com>
2019-08-16 11:50:30 -0700

    paging js in the works

---
+++ src/services/pokedex-api.js
@@ -0,0 +1,26 @@
+const URL = 'https://alchemy-pokedex.herokuapp.com/api/pokedex';
+
+export function getPokedexAPI(options) {
+
+    const page = options.page || 1;
+    const search = options.search;
+
+    const url = `${URL}?page=${page || 1}&search=${search || ''}`;
+
+    return fetch(url)
+        .then(response => response.json())
+
+        .then(results => {
+            return {
+                count: 50 * page,
+                results: results
+            };
+        });
+}
+
+
+// .then(data => {
+//     const pokemonList = new PokemonList({ pokemon: data });
+//     const pokemonListDOM = pokemonList.renderDOM();
+//     const pokemonSelection = dom.querySelector('.render-cards-here');
+//     pokemonSelection.appendChild(pokemonListDOM);
